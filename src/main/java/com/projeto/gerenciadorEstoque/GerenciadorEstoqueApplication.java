d9242c862955900837f7806d82fceb01396ad9c1
Lucas Matheus <lucas-matheusdev@outlook.com>
2024-12-15 12:36:56 -0300

    base do projeto

---
+++ src/main/java/com/projeto/gerenciadorEstoque/GerenciadorEstoqueApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.gerenciadorEstoque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorEstoqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorEstoqueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projeto/gerenciadorEstoque/GerenciadorEstoqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.gerenciadorEstoque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorEstoqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
